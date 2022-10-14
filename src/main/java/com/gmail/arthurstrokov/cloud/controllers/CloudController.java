703489721bb628daeb3b18e1acebc48184a48e7d
arthurstrokov <arthurstrokov@gmail.com>
2022-10-14 11:00:30 +0300

    up add devtools
    up application-cloud.yml

---
+++ src/main/java/com/gmail/arthurstrokov/cloud/controllers/CloudController.java
@@ -0,0 +1,23 @@
+package com.gmail.arthurstrokov.cloud.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Артур Александрович Строков
+ * @email astrokov@clevertec.ru
+ * @created 14.10.2022
+ */
+@RestController
+public class CloudController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello!";
+    }
+
+    @GetMapping("/dev")
+    public String helloDevTools() {
+        return "Hello dev tools!";
+    }
+}
