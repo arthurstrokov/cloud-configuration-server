3bf0cc340adfb504e4616b54cece2e13f9aed945
Артур Строков <astrokov@clevertec.ru>
2022-11-22 22:51:25 +0300

    up example-app-configuration.yml up name for main class

--- src/main/java/com/gmail/arthurstrokov/cloud/Application.java
+++ src/main/java/com/gmail/arthurstrokov/cloud/Application.java
@@ -8,9 +8,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
-public class CloudConfigurationApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(CloudConfigurationApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
